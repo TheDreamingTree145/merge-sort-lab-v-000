6c754e2e3ebf636b9c6a215407ffd9a0d4f9d500
Dustin Anderson <dandersonsoftware@outlook.com>
2018-01-22 02:50:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,7 @@ function mergeSort(array) {
   let midpoint = array.length/2 
   let firstHalf = array.slice(0, midpoint)
   let secondHalf = array.slice(midpoint, array.length)
+  let sorted;
+  
   
 }
