4a5c374b4922bac10bc52cd4bd925d251c387fb2
Dustin Anderson <dandersonsoftware@outlook.com>
2018-01-22 02:37:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ function findMinAndRemoveSorted(array) {
 function merge(firstHalf, secondHalf) {
   let sorted = []
   while (firstHalf.length != 0 && secondHalf.length != 0) {
-    
+    if (firstHalf[0] < secondHalf[0]) {
+      sorted.push(findMinAndRemoveSorted(firstHalf))
+    } else {
+      sorted.push(findMinAndRemoveSorted(secondHalf))
+    }
   }
+  sorted.concat(firstHalf).concat(secondHalf)
 }
